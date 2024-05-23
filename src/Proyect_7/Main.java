dea21a1fa04d54eac05d305657b889c84df2a98b
POO <jonathanrm445@gmail.com>
2024-05-23 08:35:31 -0500

    Cambio realizado

--- src/Proyect_7/Main.java
+++ src/Proyect_7/Main.java
@@ -2,6 +2,9 @@ package Proyect_7;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Como estas");
+        System.out.println("Hello wordl");
+        System.out.println("Hi");
+        System.out.println("Que mas?");
+
     }
 }
