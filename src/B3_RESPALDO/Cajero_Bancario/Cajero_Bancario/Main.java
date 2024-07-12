ad3bb479d3ab83d8b1293d40dbe0cdadfa3ae62e
Jonathan Ramirez <jonathanrm445@gmail.com>
2024-07-11 19:27:39 -0500

    .

--- src/B3_RESPALDO/Cajero_Bancario/Cajero_Bancario/Main.java
+++
@@ -1,9 +0,0 @@
-package B3_RESPALDO.Cajero_Bancario.Cajero_Bancario;
-
-public class Main {
-    public static void main(String[] args){
-
-        Login ingreso = new Login();
-        ingreso.iniciar();
-    }
-}
